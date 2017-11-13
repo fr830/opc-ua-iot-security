b323c0d8bd16a62d09251cbf0bb86fb0e554b696
Herle <herleraja@gmail.com>
2017-11-13 16:24:20 +0100

    Adding Basic files

--- src/main/java/org/dfki/iot/attack/Application.java
+++ src/main/java/org/dfki/iot/attack/Application.java
@@ -1,10 +1,17 @@
 package org.dfki.iot.attack;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
 
+	// Create Logger
+		private static final	Logger myLogger = LoggerFactory.getLogger( Application.class ); 
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//myLogger.info("\n LocalizedMessage : "+e.getLocalizedMessage() +"\n Message :: "+e.getMessage() +"\n toString :: "+e.toString() +"\n: StackTrace :: "+e.getStackTrace());
+		
 	}
 
 }
